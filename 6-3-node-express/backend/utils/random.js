491266e17fe88c15844aac72a35b3663e711204e
HussainAlsadah <s202282700@kfupm.edu.sa>
2026-04-06 10:43:33 +0300

    some final changes

--- 6-3-node-express/backend/utils/random.js
+++ 6-3-node-express/backend/utils/random.js
@@ -1,4 +1,6 @@
 // TODO 2: Create & Implement getRandomInt function
 import { getRandomQuote } from "./quotes.js";
 
-return Math.floor(Math.random() * (max - min + 1)) + min;
+export function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
